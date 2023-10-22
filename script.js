8b74607c1fa7c1283c98681cd31d8a4dbad5c068
Alen Babu <alenbabualen@gmail.com>
2023-10-22 13:43:53 +0530

    js code added calculator completed

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function display(){
-    alert("WELCOME");
+
+function clearDisplay(){
+    document.getElementById("screen").value="";
+}
+
+function displayKey(key){
+    document.getElementById("screen").value=document.getElementById("screen").value+key;
+    
+}
+function resultDisplay(){
+    var result=eval(document.getElementById("screen").value);
+    document.getElementById("screen").value=result;
+
 }
